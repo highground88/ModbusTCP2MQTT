fbe5b932b931f4987604ad7c938e28eb179e6b17
highground88 <emaillj@hotmail.com>
2021-09-10 13:09:10 +1000

    Update modbus-sungrow-sg8kd.py

--- modbus-sungrow-sg8kd.py
+++ modbus-sungrow-sg8kd.py
@@ -11,6 +11,22 @@ read_register = {
   "5022": "inverter_current_10",    # A
   "5031": "total_active_power",     # W
   "5036": "grid_frequency_10"       # Hz
+  "5071": "array_insulation_resistance_10",  	# k-ohm XX
+  "5113": "daily_operation_time",  				# m XX
+  "5128": "monthly_power_yield_0.01",			# Wh XX
+  
+  "5083": "export_power_overflow",  # W - House Grid Consumption (+ = importing, - = exporting)
+  "5084": "export_power_indicator", # W - House Grid Consumption Overflow Indicator
+  "5091": "power_meter",            # W - House Overall Consumption
+
+  "5097": "daily_purchased_energy_10", # kW
+  "5101": "daily_energy_consumption_0.01", # Wh
+  "5103": "total_energy_consumption_10",   # kWh
+  
+  "5216": "export_power_overflow",  # W - House Grid Consumption (+ = importing, - = exporting) #new read register
+  "5217": "export_power_indicator", # W - House Grid Consumption Overflow Indicator				#new read register
+  "5218": "power_meter",            # W - House Overall Consumption		
+  
 }
 
 holding_register = {
